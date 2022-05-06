fff48aa581029639a27a3e4e8ee2003386f3e6e1
Julian <julian.driebeek@stonybrook.edu>
2022-05-05 21:36:35 -0400

    Code can presently solve individual cells

---
+++ driver.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include<iomanip>
+#include <charconv>
+
+
+#include "cell.H"
+#include "board.H"
+
+int main(){
+  Board a;
+
+  std::string file {"board.dat"};
+  int iter=0;
+ 
+   a=a.read(file);
+ 
+   std::cout<<a<<std::endl;
+ 
+   int x=1;
+   int y=7;
+   cell c (a.nums[x+9*y],x,y,true);
+   std::vector<char> e=c.forbid(a);
+   cell d =   c.solve(a);
+
+   std::cout<<c<<std::endl;
+   std::cout<<d<<std::endl;
+   
+
+   
+
+   for(int i=0;i<e.size();i++){
+
+      std::cout<<e[i]<<std::endl;
+     } 
+    
+  
+}
+//mark original cells as true make others false and different color
