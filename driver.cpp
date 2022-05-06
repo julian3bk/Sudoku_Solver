09332d59d5d48c72e2ebbaa49c15edbe9f521bce
Julian <julian.driebeek@stonybrook.edu>
2022-05-06 02:29:17 -0400

    Now able to solve puzzles

--- driver.cpp
+++ driver.cpp
@@ -9,35 +9,44 @@
 
 #include "cell.H"
 #include "board.H"
+#include "solver.H"
 
-int main(){
+int main(int argc, char *argv[]){
   Board a;
+  std::string file;
 
-  std::string file {"board.dat"};
+  std::cout<<argc;
+  if(argc==2){
+     file=argv[1];
+  }
+  else{
+      std::cout<<"which game board?"<<std::endl;
+    std::cin>>file;
+  }
+
   int iter=0;
  
    a=a.read(file);
  
    std::cout<<a<<std::endl;
  
-   int x=1;
-   int y=7;
-   cell c (a.nums[x+9*y],x,y,true);
-   std::vector<char> e=c.forbid(a);
-   cell d =   c.solve(a);
-
-   std::cout<<c<<std::endl;
-   std::cout<<d<<std::endl;
-   
-
   
+   solver slvr(a);
+    Board solved=slvr.bsolve();
 
+
+  int x=0;
+   int y=7;
+   cell c (solved.nums[x+9*y],x,y,true);
+   std::vector<char> e=c.required(a);
+   cell d =   c.solve(solved);
+
+   //std::cout<<c<<std::endl;
+   //std::cout<<d<<std::endl;
    for(int i=0;i<e.size();i++){
 
-     //  std::cout<<e[i]<<std::endl;
-     } 
-     Board solved=a.bsolve();
-     std::cout<<solved<<std::endl;
-  
+     // std::cout<<e[i]<<std::endl;
+     }
+
 }
 //mark original cells as true make others false and different color
