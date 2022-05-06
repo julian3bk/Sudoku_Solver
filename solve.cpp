8dd4ff42da495e125d3ceb98685a57ef41269479
Julian <julian.driebeek@stonybrook.edu>
2022-05-06 05:12:21 -0400

    Seperated functional program from driver

---
+++ solve.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include<iomanip>
+#include <charconv>
+
+
+#include "cell.H"
+#include "board.H"
+#include "solver.H"
+
+int main(int argc, char *argv[]){
+  Board board;
+  std::string file;
+
+  std::cout<<argc;
+  if(argc==2){
+     file=argv[1];
+  }
+  else{
+      std::cout<<"which game board?"<<std::endl;
+    std::cin>>file;
+  }
+
+  int iter=0;
+ 
+   board=board.read(file);
+ 
+   std::cout<<board<<std::endl;
+ 
+  
+   solver slvr(board);
+    Board solved=slvr.bsolve();
+
+
+
+}
